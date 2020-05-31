86fac52206d807fd300c06ea69c4284e7d076b28
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2020-05-31 18:55:40 +0200

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 app = express()
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
 
