b7f49bea6ef994d7961aec02ffa626a775e3a627
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2020-05-31 20:03:54 +0200

    3.11 phonebook full stack

---
+++ build/precache-manifest.a6d725395e252ca6377b43d93e0dc151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce2482977fb311ab9b3a2ea7c3dfbbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209e5d58ceb3ee6bf0d3",
+    "url": "/static/css/main.7497aa2a.chunk.css"
+  },
+  {
+    "revision": "49bdc01c5b65f8d5ccd5",
+    "url": "/static/js/2.3b9df0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b9df0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209e5d58ceb3ee6bf0d3",
+    "url": "/static/js/main.bbfaba6f.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
 
